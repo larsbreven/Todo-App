5e9b28e4bece6ddd226be9274aa041a6bbb65985
Lars Brevén <lars.breven@telia.com>
2021-03-09 21:04:19 +0100

    Person class ready, but not tested.

---
+++ Todo App/Model/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo_App.Model
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private readonly int personId;
+
+        public Person(string firstName, string lastName, int personId)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.personId = personId;
+        }
+        public string FirstName                         // Public string name property
+        {
+            get     // If somebody wants to access this FirstName property they will get firstname in return, acts as a get-method
+            {
+                return firstName;   // If somebody try to access from outside they will get what is in this code block, firstName
+            }
+            set
+            {
+
+                if (string.IsNullOrEmpty(value))    // Indicates whether the specified string is Null or an empty string "" 
+                {
+                    firstName = value;              // This value is only valid when we are inside the property "set"
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))    // Indicates whether the specified string is Null or an empty string "" 
+                {
+                    lastName = value;
+                }
+            }
+        }
+    }
+}
--- Todo App/Program.cs
+++ Todo App/Program.cs
@@ -7,6 +7,7 @@ namespace Todo_App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
